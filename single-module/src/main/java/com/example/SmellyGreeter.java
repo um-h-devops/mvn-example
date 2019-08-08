d6b3914cdadff50b280c8982baae10b552c9da12
John Walsh <mjohnw@med.umich.edu>
2019-08-08 12:46:46 -0400

    [ADD] SmellyGreeter

---
+++ single-module/src/main/java/com/example/SmellyGreeter.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class SmellyGreeter {
+
+  /**
+   * This is a constructor.
+   */
+  public SmellyGreeter() {
+
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+
