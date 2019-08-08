620377dcc3d9430e8c25af1931ea804ccb92b826
John Walsh <mjohnw@med.umich.edu>
2019-08-08 14:02:09 -0400

    [IMP] smelly fun

--- single-module/src/main/java/com/example/SmellyGreeter.java
+++ single-module/src/main/java/com/example/SmellyGreeter.java
@@ -15,6 +15,7 @@ public class SmellyGreeter {
   //TODO: Add javadoc comment
   public String greet(String someone) {
     String greeter_name = someone;
+    String greeter_name = someone;
     String greeter_smelly_name = greeter_name;
     return String.format("Hello, %s!", greeter_smelly_name);
   }
